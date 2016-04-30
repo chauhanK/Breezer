c2cd4e30245e91be87da633a11cc45e064a558ab
karan chauhan <karan chauhan>
2016-05-01 00:11:04 +0530

    displayed contents by consuming rest webservice

--- app.js
+++ app.js
@@ -1,7 +1,28 @@
 
-	var.app=angular.module('first',[]);
+	var app=angular.module('first',[]);
 
-	app.controller('myCtrl',function($scope){
-		$scope.body="Hello World";
+	app.controller('mySongs',function($scope, $http) {
+		$http.get('http://192.168.0.8:8080/api/frontend/songs')
+		.success(function(data) {
+            $scope.songs = data;
+            
+		});
 	});
+
+	app.controller('myVideos',function($scope, $http) {
+		$http.get('http://192.168.0.8:8080/api/frontend/videos')
+		.success(function(data) {
+            $scope.videos = data;
+
+      });
+	});
+
+	app.controller('myBooks',function($scope, $http) {
+		$http.get('http://192.168.0.8:8080/api/frontend/books')
+		.success(function(data) {
+            $scope.books = data;
+
+      });
+	});
+
 
